640aa02392c45dd4a2bec144155560964c0d2bf9
alex-mal79 <a-79maleev@yandex.ru>
2022-01-02 13:50:18 +0300

    доабавлены разделы "О компании", "Наши услуги", fullscreen на главную

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,43 @@
-
+//================================================================
+//Слайдер Swiper
+//================================================================
+// Слайдер full screen
+const fullSlider = document.querySelector('.fullscreen__swiper');
+if(fullSlider){
+    const fullSwiper = new Swiper(fullSlider,{
+        observer: true,
+        observerParents: true,
+        observerSlideChildren: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            prevEl: '.fullscreen__button-prev',
+            nextEl: '.fullscreen__button-next',
+        },
+        pagination: {
+            el: '.fullscreen__progress',
+            type: 'progressbar',
+        },
+        thumbs:{
+            swiper:{
+                el: '.thumbs-fullscreen__swiper',
+                slidesPerView: 1,
+                spaceBetween: 10,
+                freeMode: true,
+                watchSlidesProgress: true,
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,  
+                        slidesPerGroup: 1,              
+                    },
+                    600: {
+                        slidesPerView: 2,  
+                    },
+                    840: {
+                        slidesPerView: 3,
+                    },
+                },
+            }
+        },
+    });
+}
